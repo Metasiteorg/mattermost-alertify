8ed171fd0f7ff1bddfba81332ec24156eedaf2bb
Aurimas Bachanovičius <aurimas.bachanovicius@metasite.net>
2021-02-17 13:55:03 +0200

    spaces

--- index.js
+++ index.js
@@ -1,95 +1,95 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const artifact = require('@actions/artifact');
-const axios = require('axios');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const artifact = require('@actions/artifact')
+const axios = require('axios')
 const fs = require('fs')
 const artifactClient = artifact.create()
 const xml2js = require('xml2js')
 
 const config = {
-    headers: {
-        'authorization': `Bearer ${core.getInput('git_token')}`,
-        'content-type': 'application/json'
+  headers: {
+    'authorization': `Bearer ${core.getInput('git_token')}`,
+    'content-type': 'application/json'
+  }
+}
+
+async function getHTMLUrl (apiUrl) {
+  const response = await axios.get(apiUrl, config)
+  return response.data.html_url
+}
+
+async function getCommits () {
+  try {
+    const response = await axios.get(github.context.payload.pull_request.commits_url, config)
+
+    let commits = []
+    for (const item of response.data) {
+      let url = await getHTMLUrl(item.url)
+      commits.push(`:commit: ${item.committer.login}: [${item.commit.message}](${url})`)
     }
-};
 
-async function getHTMLUrl(apiUrl) {
-    const response = await axios.get(apiUrl, config);
-    return response.data.html_url
+    return commits
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-async function getCommits() {
-    try {
-        const response = await axios.get(github.context.payload.pull_request.commits_url, config);
-
-        let commits = [];
-        for (const item of response.data) {
-            let url = await getHTMLUrl(item.url);
-            commits.push(`:commit: ${item.committer.login}: [${item.commit.message}](${url})`);
-        }
-
-        return commits;
-    } catch (err) {
-        console.error(err)
-    }
+async function coverage () {
+  const downloadResponse = await artifactClient.downloadArtifact('code-coverage-report', 'coverage.txt')
+  return fs.readFileSync(downloadResponse.downloadPath, 'utf8')
 }
 
-async function coverage() {
-    const downloadResponse = await artifactClient.downloadArtifact('code-coverage-report', 'coverage.txt')
-    return fs.readFileSync(downloadResponse.downloadPath, 'utf8');
-}
-
-async function tests() {
-    const downloadResponse = await artifactClient.downloadArtifact('tests-junit', 'junit.xml')
-    const junit = fs.readFileSync(downloadResponse.downloadPath, 'utf8')
-    return xml2js.parseStringPromise(junit).then(function (result) {
-        const meta = result.testsuites.testsuite[0].$
-        return `Tests: ${meta.tests} Assertions: ${meta.assertions} Errors: ${meta.errors} Warnings: ${meta.warnings} Failures: ${meta.failures} Skipped: ${meta.skipped} Time: ${meta.time}`;
-    });
+async function tests () {
+  const downloadResponse = await artifactClient.downloadArtifact('tests-junit', 'junit.xml')
+  const junit = fs.readFileSync(downloadResponse.downloadPath, 'utf8')
+  return xml2js.parseStringPromise(junit).then(function (result) {
+    const meta = result.testsuites.testsuite[0].$
+    return `Tests: ${meta.tests} Assertions: ${meta.assertions} Errors: ${meta.errors} Warnings: ${meta.warnings} Failures: ${meta.failures} Skipped: ${meta.skipped} Time: ${meta.time}`
+  })
 
 }
 
-async function codeQuality() {
-    const downloadResponse = await artifactClient.downloadArtifact('code-quality', 'codequality.txt')
-    return fs.readFileSync(downloadResponse.downloadPath, 'utf8');
+async function codeQuality () {
+  const downloadResponse = await artifactClient.downloadArtifact('code-quality', 'codequality.txt')
+  return fs.readFileSync(downloadResponse.downloadPath, 'utf8')
 }
 
-async function generateMessage() {
-    const repoName = `[${github.context.payload.repository.name}](${github.context.payload.repository.html_url})`
-    const prName = `[${github.context.payload.pull_request.title}](${github.context.payload.pull_request.html_url})`
-    const status = "success"
-    const color = status === "success" ? "#00FF00" : "#FF0000"
-    const branch = `from **${process.env.GITHUB_HEAD_REF}** to **${process.env.GITHUB_BASE_REF}**`
+async function generateMessage () {
+  const repoName = `[${github.context.payload.repository.name}](${github.context.payload.repository.html_url})`
+  const prName = `[${github.context.payload.pull_request.title}](${github.context.payload.pull_request.html_url})`
+  const status = 'success'
+  const color = status === 'success' ? '#00FF00' : '#FF0000'
+  const branch = `from **${process.env.GITHUB_HEAD_REF}** to **${process.env.GITHUB_BASE_REF}**`
 
-    return {
-        "text": `### ${github.context.workflow} ${prName} ${status} ###`,
-        "username": "Uncle Github",
-        "attachments": [
-            {
-                "color": "" + color + "",
-                "fields": [
-                    {"short": true, "title": ":github: Repository:", "value": repoName},
-                    {"short": true, "title": ":docker: Image name:", "value": "${image_name}"},
-                    {"short": true, "title": ":git: Branch name", "value": "" + branch + ""},
-                    {"short": true, "title": ":phpunit: Tests", "value": (await tests())},
-                    {
-                        "short": true,
-                        "title": ":coverage: Tests Coverage",
-                        "value": "```\n" + (await coverage()) + "\n```"
-                    },
-                    {"short": true, "title": ":phpcs: Code Quality", "value": (await codeQuality())},
-                    {"short": false, "title": ":commits: Commits", "value": (await getCommits()).join("\n")}
-                ]
-            }]
-    }
+  return {
+    'text': `### ${github.context.workflow} ${prName} ${status} ###`,
+    'username': 'Uncle Github',
+    'attachments': [
+      {
+        'color': '' + color + '',
+        'fields': [
+          { 'short': true, 'title': ':github: Repository:', 'value': repoName },
+          { 'short': true, 'title': ':docker: Image name:', 'value': '${image_name}' },
+          { 'short': true, 'title': ':git: Branch name', 'value': '' + branch + '' },
+          { 'short': true, 'title': ':phpunit: Tests', 'value': (await tests()) },
+          {
+            'short': true,
+            'title': ':coverage: Tests Coverage',
+            'value': '```\n' + (await coverage()) + '\n```'
+          },
+          { 'short': true, 'title': ':phpcs: Code Quality', 'value': (await codeQuality()) },
+          { 'short': false, 'title': ':commits: Commits', 'value': (await getCommits()).join('\n') }
+        ]
+      }]
+  }
 }
 
 generateMessage().then((msg) => {
-    const webhook = core.getInput('mattermost_webhook');
-    axios.post(webhook, msg)
-        .then(function (response) {
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+  const webhook = core.getInput('mattermost_webhook')
+  axios.post(webhook, msg)
+    .then(function (response) {
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 })
