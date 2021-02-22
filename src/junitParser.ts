2b89efd831538bd02b74efe845794b2dd08ba071
Aurimas Bachanovičius <aurimas.bachanovicius@metasite.net>
2021-02-22 10:18:58 +0200

    to table junit results

--- src/junitParser.ts
+++ src/junitParser.ts
@@ -3,12 +3,10 @@ import xml2js from 'xml2js'
 export function parseJunit(junit: string): Promise<string> {
   return xml2js.parseStringPromise(junit).then(result => {
     const meta = result.testsuites.testsuite[0].$
-    return `**Tests**: ${meta.tests}\n
-**Assertions**: ${meta.assertions}\n
-**Errors**: ${meta.errors}\n
-**Warnings**: ${meta.warnings} \n 
-**Failures**: ${meta.failures} \n 
-**Skipped**: ${meta.skipped}\b
-**Time**: ${meta.time}`
+
+    return `
+    | Tests | Assertions | Errors | Warnings | Failures | Skipped | Time |
+|-------|------------|------------|----------|----------|---------|------|
+| ${meta.tests} | ${meta.assertions} | ${meta.errors} | ${meta.warnings} | ${meta.failures} | ${meta.skipped} | ${meta.time} |`
   })
 }
